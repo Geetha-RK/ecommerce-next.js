1f44178fffe004406f5219fe9ac3b67a7ab3f6d9
Geetha R Kumar <geetha2work@gmail.com>
2024-12-05 14:06:15 -0500

    Add Cart page and 404 page

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -1,14 +1,15 @@
 import { Product } from './product-data';
 import Image from 'next/image';
+import Link from 'next/link';
 export default function ProductsList({ products }:{ products: Product[] }){
     return (
         <div>
             {products.map(product => (
-                <div key={product.id}>
+                <Link key={product.id} href={'/products/'+ product.id}>
                     <Image src={'/' + product.imageURL} alt="Product-Image" width={150} height={150}></Image>
                     <h2>{product.name}</h2>
                     <p>${product.price}</p>
-                </div>
+                </Link>
             ))}
         </div>
     )
